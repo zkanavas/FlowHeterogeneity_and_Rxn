24531a94a1ebdd5f2b5809bec7753a13f9d7f4d5
Zoe Kanavas <zkanavas@ucdavis.edu>
2021-06-17 13:18:02 -0700

    generalized visualization script

--- visualizevelocityfield.py
+++ visualizevelocityfield.py
@@ -12,8 +12,8 @@ plot_structure = False
 plot_velocity_regions = False
 plot_percolating_path = True
 
-sample_descriptor = "beadpack"
-imagesize =(500,500,500)
+sample_descriptor = "estaillades"
+imagesize =(650,650,650)
 datatype = 'uint8'
 
 #data directory
@@ -28,10 +28,10 @@ npimg = npimg.reshape(imagesize)
 
 if plot_velocity_field == True:
     #get image data in correct format
-    coords,color= array_to_dataframe(npimg.astype('float16'),sliceby=1)
-    coords,color = convert_to_structure(coords,color,show_pore_space=True)
+    coords,color= array_to_dataframe(npimg.astype('float32'),sliceby=1)
     # create/load .ply file and visualize
-    pcd = numpy_to_ply(coords,color,file_name="beadpack_vel_mag.ply", overwrite=True, colormap="turbo")
+    filename = directory + "/" + sample_descriptor + "_vel_mag.ply"
+    pcd = numpy_to_ply(coords,color,file_name=filename, overwrite=False, colormap="turbo")
     pcd = pcd.uniform_down_sample(every_k_points=5)
     o3d.visualization.draw_geometries([pcd])
 
@@ -40,13 +40,14 @@ elif plot_structure == True:
     coords_bin,color_bin = array_to_dataframe(npimg.astype('float32'),sliceby=1)
     #convert velocity field into structure only (binarize) and remove non-grain voxels
     coords_bin,color_bin = convert_to_structure(coords_bin,color_bin)
+    filename = directory + "/" + sample_descriptor + "_structure.ply"
     # create/load .ply file and visualize
-    pcd = numpy_to_ply(coords_bin,color_bin,file_name="structure.ply", overwrite=True, colormap="binary")
+    pcd = numpy_to_ply(coords_bin,color_bin,file_name=filename, overwrite=False, colormap="binary")
     o3d.visualization.draw_geometries([pcd])
 
 elif plot_velocity_regions == True:
     #get image data in correct format
-    coords,color = array_to_dataframe(npimg.astype('float32'),sliceby=10)
+    coords,color = array_to_dataframe(npimg.astype('float32'),sliceby=5)
     # coords,color = only_percolating_path(coords,color)
     # create/load .ply file and visualize
     filename = directory + "/" + sample_descriptor + "_velocity_region.ply"
@@ -55,10 +56,10 @@ elif plot_velocity_regions == True:
 
 elif plot_percolating_path == True:
     #get image data in correct format
-    coords,color = array_to_dataframe(npimg.astype('float32'),sliceby=1)
+    coords,color = array_to_dataframe(npimg.astype('float32'),sliceby=10)
     #remove everything except for percolating path
     coords,color = only_percolating_path(coords,color)
     # create/load .ply file and visualize
     filename = directory + "/" + sample_descriptor + "_percolating_path.ply"
-    pcd = numpy_to_ply(coords, color, file_name=filename, overwrite=True, colormap="segmented")
+    pcd = numpy_to_ply(coords, color, file_name=filename, overwrite=False, colormap="segmented")
     o3d.visualization.draw_geometries([pcd])
