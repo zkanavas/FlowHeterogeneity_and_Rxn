9dac0ba3c57c43273cb2fb3a97198daa03444601
Zoe Kanavas <zkanavas@ucdavis.edu>
2021-08-17 14:56:21 -0700

    percolationthresholdfix

--- velocity_thresholding.py
+++ velocity_thresholding.py
@@ -5,7 +5,7 @@ from skimage.measure import label, regionprops, regionprops_table
 import pandas as pd
 import time
 
-tic = time.perf_counter()
+# tic = time.perf_counter()
 
 #customize these for each sample
 # sample_descriptor = "menke_ketton"
@@ -19,15 +19,15 @@ tic = time.perf_counter()
 # imagesize =(998,998,800)
 # sample_descriptor = "alkhulafi_silurian"
 # imagesize = (946, 946, 390)
-sample_descriptor = "beadpack"
-imagesize = (500,500,500)
-datatype = 'float16'
+sample_descriptor = "menke_2017_est"
+imagesize = (998,998,800)
+datatype = 'float32'
 
 #data directory
 directory = os.path.normpath(r'E:\FlowHet_RxnDist')
 
 #data file location
-vel_magnitude_file = directory + "/" + sample_descriptor + "_velocity_magnitude.txt"
+vel_magnitude_file = directory + "/" + sample_descriptor + "_velocity_magnitude.raw"
 
 #load images
 vel_magnitude = np.fromfile(vel_magnitude_file, dtype=np.dtype(datatype)) 
@@ -36,26 +36,29 @@ vel_magnitude = vel_magnitude.reshape(imagesize)
 mean = np.mean(vel_magnitude[vel_magnitude != 0])
 vel_normalized = np.divide(vel_magnitude,mean)
 percolation_threshold = np.max(vel_normalized)#*0.02
-# percolation_threshold = 2.07
 
-# continuous = False
-# while not continuous:
-#     percolation_threshold -= 0.01
-#     print(percolation_threshold)
-#     [x_2,y_2,z_2] = np.where(vel_normalized >= percolation_threshold)
-#     vel_norm[x_2,y_2,z_2] = 1
-
-#     labels_out = label(vel_norm)
-#     props = regionprops_table(labels_out,properties =['label','bbox'])
-#     props = pd.DataFrame(props)
-#     checking_bounds = props['bbox-5'][props['bbox-2']==0] == imagesize[2]
-#     if any(checking_bounds):
-#         print('done')
-#         props = regionprops_table(labels_out,properties =['label','bbox','coords','area'])
+# percolation_threshold = 2.3
+# interval = np.logspace(1e-5,1,6)
+# for i in interval:
+#     continuous = False
+#     vel_norm = np.zeros(vel_magnitude.shape)
+#     while not continuous:
+#         print(percolation_threshold)
+#         [x_2,y_2,z_2] = np.where(vel_normalized >= percolation_threshold)
+#         vel_norm[x_2,y_2,z_2] = 1
+#         labels_out = label(vel_norm)
+#         props = regionprops_table(labels_out,properties =['label','bbox'])
 #         props = pd.DataFrame(props)
-#         id_box = props['bbox-5'][props['bbox-2']==0] == imagesize[2]
-#         coords = props['coords'][id_box.index[np.where(id_box)]].tolist()
-#         continuous = True
+#         checking_bounds = props['bbox-5'][props['bbox-2']==0] == imagesize[2]
+#         if any(checking_bounds):
+#             print('done')
+#             props = regionprops_table(labels_out,properties =['label','bbox','area'])
+#             props = pd.DataFrame(props)
+#             id_box = props['bbox-5'][props['bbox-2']==0] == imagesize[2]
+#             print(percolation_threshold,props['area'][id_box.index[np.where(id_box)]])
+#             # coords = props['coords'][id_box.index[np.where(id_box)]].tolist()
+#             continuous = True
+#         else: percolation_threshold -= 0.01
 # #id velocity zones
 # vel_norm[x_2,y_2,z_2] = 2 #disconnected high velocity region
 # vel_norm[coords[0][:,0],coords[0][:,1],coords[0][:,2]] = 3 #percolating path
@@ -83,6 +86,7 @@ def save(vel_normalized,percolation_threshold):
     props = regionprops_table(labels_out,properties =['label','bbox','coords','area'])
     props = pd.DataFrame(props)
     id_box = props['bbox-5'][props['bbox-2']==0] == imagesize[2]
+    # print(percolation_threshold,props['area'][id_box.index[np.where(id_box)]])
     coords = props['coords'][id_box.index[np.where(id_box)]].tolist()
     #id velocity zones
     vel_norm[x_2,y_2,z_2] = 2 #disconnected high velocity region
@@ -94,19 +98,25 @@ def save(vel_normalized,percolation_threshold):
     #save thresholded velocity field
     vel_norm.astype('uint8').tofile(directory +"/" + sample_descriptor + '_velocity_regions.txt')
 
+# interval = np.logspace(-5,0,6)
+i = 1e-4
+# for i in interval:
+tic = time.perf_counter()
+percolation_threshold = np.max(vel_normalized)#*0.02
 upper_pt = percolation_threshold
 lower_pt = upper_pt/2
 stop = False
 while not stop:
+    print(upper_pt,lower_pt)
     if not checkbounds(vel_normalized,lower_pt):
-        upper_pt = lower_pt
-        lower_pt = upper_pt/2
+        upper_pt_new = lower_pt
+        lower_pt -= (upper_pt-lower_pt)/2 #FIX
+        upper_pt = upper_pt_new
     else:
-        if upper_pt - lower_pt < 1e-5:
-            print(lower_pt)
+        if upper_pt - lower_pt < i:#1e-5:
+            print('final: ',lower_pt)
             save(vel_normalized,lower_pt)
             stop=True
-        else: lower_pt += lower_pt/2
-
-toc = time.perf_counter()
-print("time elapsed: " + str(toc-tic) + " seconds" )
+        else: lower_pt += (upper_pt-lower_pt)/2
+    toc = time.perf_counter()
+    print(i, "time elapsed: " + str(toc-tic) + " seconds" )
