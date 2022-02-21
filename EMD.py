15dc58ae6e7ba52c5df0355314c6c0e451862230
Zoe Kanavas <zkanavas@ucdavis.edu>
2022-02-21 15:36:21 -0800

    updated EMD functions

--- EMD.py
+++ EMD.py
@@ -3,6 +3,7 @@ import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
+import os
 
 def resample_vel_distribution(vel_magnitude):
     new_array = np.ones(1e6)
@@ -16,11 +17,17 @@ def dx_mean_std(vel_magnitude):
 
     mean = np.sum(vel_magnitude[0]*vel_magnitude[1]*dx)/np.sum(dx*vel_magnitude[1])
     std = ((np.sum((vel_magnitude[0]-mean)**2))/len(vel_magnitude[0]))**(1/2)
-    AUC = np.sum(dx*vel_magnitude[1])
-    return dx,AUC, mean, std
+    auc = np.sum(dx*vel_magnitude[1])
+    return dx,auc, mean, std
+
+def mean_velocity(filename,datatype):
+    vel_magnitude = np.fromfile(filename, dtype=np.dtype(datatype)) 
+    # vel_magnitude = vel_magnitude.reshape(imagesize)
+    mean = np.mean(vel_magnitude[vel_magnitude != 0])
+    return mean
 
 plot_ = False
-calc_metric = False
+calc_metric = True
 # samples = ["menke_2017_est","menke_2017_ket","menke_2017_ket36","menke_2017_portland"]
 # samples = ["Sil_HetA_High","Sil_HetA_Low","Sil_HetB_High","Sil_HetB_Low"]
 df = pd.read_csv("flow_transport_rxn_properties.csv",header=0,index_col=0)
@@ -29,30 +36,61 @@ samples = df.index
 samples = ["AH","AL","BH","BL"]
 normfreqsamples = ["AH","AL","BH","BL","Sil_HetA_High_Scan1","Sil_HetA_Low_Scan1","Sil_HetB_High_Scan1","Sil_HetB_Low_Scan1"]
 
-down_dir = r"C:\Users\zkanavas\Downloads"
-for samp in samples:
-    vel_magnitude_before1 = pd.read_csv(samp+"_before.csv",header=None)
-    vel_magnitude_before2 = pd.read_csv(down_dir+"/"+samp+"_before.csv",header=None)
-    # vel_magnitude_after = pd.read_csv(samp+"_after.csv",header=None)
-    # if samp in normfreqsamples:
-    #     if samp == "AH" :
-    #         vel_magnitude_before[0]*=0.113
-    #         # vel_magnitude_after[0]*=0.214
-    #     elif samp == "AL":
-    #         vel_magnitude_before[0]*=0.099
-    #         # vel_magnitude_after[0]*=0.153
-    #     elif samp == "BH":
-    #         vel_magnitude_before[0]*=0.080
-    #         # vel_magnitude_after[0]*=0.180
-    #     elif samp == "BL":
-    #         vel_magnitude_before[0]*=0.079
-            # vel_magnitude_after[0]*=0.130
-    fig,ax = plt.subplots()
-    ax.plot(vel_magnitude_before1[0],vel_magnitude_before1[1])
-    ax.plot(vel_magnitude_before2[0],vel_magnitude_before2[1])
-    ax.semilogx()
-    ax.set_title(samp)
-plt.show()
+directory = os.path.normpath(r'F:\FlowHet_RxnDist')
+datatype = 'float32'
+
+# for samp in samples:
+#     vel_magnitude_file = directory + "/" + samp + "_velocity_magnitude.raw"
+#     # imagesize = (df.loc[samp,'nx'],df.loc[samp,'ny'],df.loc[samp,'nz'])
+#     if samp == "AH":
+#         mean = 2.57e-7
+#         phi = 0.113
+#     elif samp == "AL":
+#         mean = 5.95e-8
+#         phi = 0.099
+#     elif samp == "BH":
+#         mean = 3.93e-8
+#         phi = 0.080
+#     elif samp == "BL":
+#         mean = 2.01e-8
+#         phi = 0.079
+#     print(samp,mean*phi)
+
+# down_dir = r"C:\Users\zkanavas\Downloads"
+# down_dir = r"C:\Users\zkana\Downloads"
+# for samp in samples:
+#     vel_magnitude_before1 = pd.read_csv(samp+"_before.csv",header=None)
+#     vel_magnitude_before2 = pd.read_csv(down_dir+"/"+samp+"_before.csv",header=None)
+#     # vel_magnitude_after = pd.read_csv(samp+"_after.csv",header=None)
+#     if samp in normfreqsamples:
+#         if samp == "AH" :
+#             vel_magnitude_before1[0]*=0.113
+#             vel_magnitude_before2[0]*=0.113
+#             # vel_magnitude_after[0]*=0.214
+#         elif samp == "AL":
+#             vel_magnitude_before1[0]*=0.099
+#             vel_magnitude_before2[0]*=0.099
+#             # vel_magnitude_after[0]*=0.153
+#         elif samp == "BH":
+#             vel_magnitude_before1[0]*=0.080
+#             vel_magnitude_before2[0]*=0.080
+#             # vel_magnitude_after[0]*=0.180
+#         elif samp == "BL":
+#             vel_magnitude_before1[0]*=0.079
+#             vel_magnitude_before2[0]*=0.079
+#             # vel_magnitude_after[0]*=0.130
+
+#     dx,auc1, mean, std=dx_mean_std(vel_magnitude_before1)
+
+#     dx,auc2, mean, std=dx_mean_std(vel_magnitude_before2)
+#     print(samp,auc1,auc2)
+#     fig,ax = plt.subplots()
+#     # ax.plot(vel_magnitude_before1[0],vel_magnitude_before1[1],label="simple")
+#     ax.plot(vel_magnitude_before2[0],vel_magnitude_before2[1],label="detailed",marker="o")
+#     ax.legend()
+#     ax.semilogx()
+#     ax.set_title(samp)
+# plt.show()
     # print(dx_mean_std(vel_magnitude_before))
 
 
@@ -61,8 +99,6 @@ after_distances = []
 before_after_distances = []
 if calc_metric == True:
     for samp in samples:
-        if samp == "AL":
-            print('check')#continue
         vel_magnitude_before = pd.read_csv(samp+"_before.csv",header=None)
         vel_magnitude_after = pd.read_csv(samp+"_after.csv",header=None)
         if samp in normfreqsamples:
@@ -93,39 +129,48 @@ if calc_metric == True:
 
         distances = []
         
-        dx_before = [vel_magnitude_before[0][ind]-vel_magnitude_before[0][ind-1] for ind in range(1,len(vel_magnitude_before[0]))]
-        dx_before = np.insert(dx_before, 0,dx_before[0])
-        if any(dx_before < 0): print(samp,"before")
+        dx_before,auc_before, mean_before, std_before=dx_mean_std(vel_magnitude_before)
+        dx_after,auc_after, mean_after, std_after=dx_mean_std(vel_magnitude_after)
+        print(samp,auc_before,auc_after)
 
-        mean_before = np.sum(vel_magnitude_before[0]*vel_magnitude_before[1]*dx_before)/np.sum(dx_before*vel_magnitude_before[1])
-        std_before = ((np.sum((vel_magnitude_before[0]-mean_before)**2))/len(vel_magnitude_before[0]))**(1/2)
+        # dx_before = [vel_magnitude_before[0][ind]-vel_magnitude_before[0][ind-1] for ind in range(1,len(vel_magnitude_before[0]))]
+        # dx_before = np.insert(dx_before, 0,dx_before[0])
+        # # if samp=="AH": print(np.exp(dx_before))
+
+        # mean_before = np.sum(vel_magnitude_before[0]*vel_magnitude_before[1]*dx_before)/np.sum(dx_before*vel_magnitude_before[1])
+        # std_before = ((np.sum((vel_magnitude_before[0]-mean_before)**2))/len(vel_magnitude_before[0]))**(1/2)
         lognormal_before = np.random.lognormal(mean=mean_before,sigma=std_before,size=vel_magnitude_before[1].size)
         
-        distance_before = stats.wasserstein_distance(vel_magnitude_before[1],lognormal_before)
+        # distance_before = stats.wasserstein_distance(vel_magnitude_before[1],lognormal_before)
 
-        dx_after = [vel_magnitude_after[0][ind]-vel_magnitude_after[0][ind-1] for ind in range(1,len(vel_magnitude_after[0]))]
-        dx_after = np.insert(dx_after, 0,dx_after[0])
-        if any(dx_after < 0): print(samp,"after")
+        # dx_after = [vel_magnitude_after[0][ind]-vel_magnitude_after[0][ind-1] for ind in range(1,len(vel_magnitude_after[0]))]
+        # dx_after = np.insert(dx_after, 0,dx_after[0])
+        # if any(dx_after < 0): print(samp,"after")
 
-        mean_after = np.sum(vel_magnitude_after[0]*vel_magnitude_after[1]*dx_after)/np.sum(dx_after*vel_magnitude_after[1])
-        std_after = ((np.sum((vel_magnitude_after[0]-mean_after)**2))/len(vel_magnitude_after[0]))**(1/2)
+        # mean_after = np.sum(vel_magnitude_after[0]*vel_magnitude_after[1]*dx_after)/np.sum(dx_after*vel_magnitude_after[1])
+        # std_after = ((np.sum((vel_magnitude_after[0]-mean_after)**2))/len(vel_magnitude_after[0]))**(1/2)
         lognormal_after = np.random.lognormal(mean=mean_after,sigma=std_after,size=vel_magnitude_after[1].size)
-        distance_after = stats.wasserstein_distance(vel_magnitude_after[1],lognormal_after)
+        # distance_after = stats.wasserstein_distance(vel_magnitude_after[1],lognormal_after)
 
-        distance_before_after = stats.wasserstein_distance(vel_magnitude_before[1],vel_magnitude_after[1])
+        # distance_before_after = stats.wasserstein_distance(vel_magnitude_before[1],vel_magnitude_after[1])
 
-        print(samp, "before AUC: ", np.sum(dx_before*vel_magnitude_before[1]))
-        print(samp, "after AUC: ", np.sum(dx_after*vel_magnitude_after[1]))
+        # print(samp, "before AUC: ", np.sum(dx_before*vel_magnitude_before[1]))
+        # print(samp, "after AUC: ", np.sum(dx_after*vel_magnitude_after[1]))
 
             # print(samp,phase,"AUC: ", np.sum(dx*vel_magnitude[1])," mean: ",mean, " std: ",std)
             # print(samp,phase,"log-normal wasserstein: ",distance)
-        before_distances.append(distance_before)
-        after_distances.append(distance_after)
-        before_after_distances.append(distance_before_after)
+        # before_distances.append(distance_before)
+        # after_distances.append(distance_after)
+        # before_after_distances.append(distance_before_after)
+        # fig,ax = plt.subplots()
+        # ax.plot(vel_magnitude_before[0],vel_magnitude_before[1])
+        # ax.plot(vel_magnitude_after[0],vel_magnitude_after[1])
+        # ax.semilogx()
+        # ax.set_title(samp)
 
     # print(samp,distances[1]-distances[0])
     # dist.append(distances)
-
+plt.show()
 if plot_ == True:
     size_min = 10
     size_max = 1000
