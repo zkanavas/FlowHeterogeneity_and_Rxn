11b7efbbf824468515aceb159aa4421670168a25
zkanavas <zkanavas@ucdavis.edu>
2022-09-08 17:24:03 -0700

    calculating rxnrate

--- SSA_porosity.py
+++ SSA_porosity.py
@@ -21,7 +21,7 @@ def append_value(dict_obj, key, value):
 
 create_sample_SSA_porosity_intime = True
 
-directory = r'F:\FlowHet_RxnDist'
+directory = r'H:\FlowHet_RxnDist'
 allsampleinfo = pd.read_csv("flow_transport_rxn_properties.csv",header=0,index_col=0)
 intermstructureinfo = pd.read_csv("Publication_intermediate_structures.csv", header=0,index_col=1)
 
@@ -31,29 +31,21 @@ else:
     sample_SSA_porosity_intime = pd.read_csv("sample_SSA_porosity_intime.csv",index_col = ["SampleName","SSA","Porosity"])
 
 for root, dirs, files in os.walk(directory):
-    if "ket0.1ph3.1" not in root: continue
+    if "PereiraNunes2016" in root: continue
+    elif "Hinz2019" in root: continue
+    elif "port0.1ph3.1" not in root: continue
     if "structures" in root:
         dirs[:] = []
         sample = [samp for samp in intermstructureinfo.index if samp in root][0]
         if sample not in sample_SSA_porosity_intime:
             sample_SSA_porosity_intime[sample] = dict()
-        
         imagesize = (allsampleinfo.nx[sample],allsampleinfo.ny[sample],allsampleinfo.nz[sample])
         voxelsize = intermstructureinfo.VoxelSize[sample]
         #grab the .raw files, compute reff using current step structure relative to former step
         num = intermstructureinfo.Steps[sample]
-        for step in range(1,num+1):
-            #Sil_HetA_High: Scan#_Porosity or _SurfaceArea
-            #Sil_HetA_Low:  Sil_HetA_LowScan#_Porosity or _SurfaceArea
-            #Sil_HetB_High: Sil_HetB_HighScan#_Porosity or _SurfaceArea
-            #Sil_HetB_Low:  Sil_HetB_HighScan#_Porosity or _SurfaceArea
-            if "AlKhulaifi" in root:
-                if sample != "Sil_HetA_High":
-                    porosityfilename = root + "/" + sample + "Scan"+str(num)+"_Porosity.gdr"
-                    surfacefilename = root + "/" + sample + "Scan"+str(num)+"_SurfaceArea.gdr"
-                elif sample == "Sil_HetA_High": 
-                    porosityfilename =  root + "/" +"Scan"+str(num)+"_Porosity.gdr"
-                    surfacefilename = root + "/" + sample + "Scan"+str(num)+"_SurfaceArea.gdr"
+        for step in range(0,num):
+            porosityfilename = root + "/Porosity"+str(step)+".gdr"
+            surfacefilename = root + "/SurfaceArea"+str(step)+".gdr"
             originalGDR     = stringmap.parseGDR(porosityfilename)
             gdrDict         = originalGDR.toRecDict()
             porosity        = float(gdrDict['ResultMap']['OpenPorosity'])/100
@@ -61,8 +53,8 @@ for root, dirs, files in os.walk(directory):
             originalGDR     = stringmap.parseGDR(surfacefilename)
             gdrDict         = originalGDR.toRecDict()
             SSA        = float(gdrDict['ResultMap']['CountVoxelSurfaces']['SpecificSurfaceArea']) #m^2/m^3
+            print(sample,step,SSA,porosity)
+            # append_value(sample_SSA_porosity_intime[sample],"SSA",SSA)
+            # append_value(sample_SSA_porosity_intime[sample],"porosity",porosity)
 
-            append_value(sample_SSA_porosity_intime[sample],"SSA",SSA)
-            append_value(sample_SSA_porosity_intime[sample],"porosity",porosity)
-
-pd.DataFrame(sample_SSA_porosity_intime).to_csv('sample_SSA_porosity_intime.csv', index=False)
+# pd.DataFrame(sample_SSA_porosity_intime).to_csv('sample_SSA_porosity_intime.csv', index=False)
