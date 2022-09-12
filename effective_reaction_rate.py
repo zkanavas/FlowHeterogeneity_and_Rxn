7eb395557a2929fad8654f57d016814110e9972e
Zoe Kanavas <zkanavas@ucdavis.edu>
2022-09-12 15:19:22 -0700

    previous step ssa

--- effective_reaction_rate.py
+++ effective_reaction_rate.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from skimage.measure import label, regionprops_table, marching_cubes, mesh_surface_area
 import csv
 
-calc_reff = False
+calc_rateofchange = False
+calc_reff = True
 plot_ = False
 
 def singlemineral_reff(rho,M_mineral,S,phi_change,time_change):
@@ -60,7 +61,7 @@ M_dolomite = 0.1844
 
 if calc_reff:
     for sample in SSAporosityinfo.index:
-        if sample != "port0.1ph3.1":continue
+        # if sample != "Sil_HetA_High":continue
         SSA = [float(ele) for ele in SSAporosityinfo.SSA[sample][1:-1].split(",")]
         porosity = [float(ele) for ele in SSAporosityinfo.Porosity[sample][1:-1].split(",")]
         timesteps = [int(ele) for ele in SSAporosityinfo.Timestep[sample][1:-1].split(",")]
@@ -69,7 +70,7 @@ if calc_reff:
         print(sample)
         for step in range(1,maxsteps,1):
             #get specific surface area, porosity change, and time change from sample_SSA_porosity_intime.csv
-            S = SSA[step]
+            S = SSA[step-1]
             phi_change = porosity[step] - porosity[step-1]
             time_change = (timesteps[step]-timesteps[step-1])*60 #in seconds
             if "AlKhulaifi" not in SSAporosityinfo.Publication[sample]:
@@ -92,47 +93,74 @@ if calc_reff:
             else: print(SSAporosityinfo.Publication[sample])
         print(reff)
 
+def moving_average(a, n=3) :
+    ret = np.nancumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+# data = pd.read_csv(r"C:\Users\zkana\Downloads\menke2017.csv",header=[0,1])
+# sampnames = ['est3.6','est3.1','ket3.6','ket3.1','port3.6','port3.1']
+data = pd.read_csv(r"C:\Users\zkana\Downloads\alkhulaifi2018.csv",header=[0,1])
+sampnames = ['HetBHigh','HetAHigh','HetALow','HetBLow']
+fig,ax = plt.subplots()
+averagewindow = 3
+for sampname in sampnames:
+    # if "ket" not in sampname: continue
+    averagedreff = moving_average(data[sampname].Y.dropna())
+    changeiny = abs(np.diff(averagedreff))/averagedreff[0]
+    changeinx = abs(np.diff(data[sampname].X.dropna()[1:-1]))
+    rateofchange = changeiny/changeinx
+    [print(sampname,data[sampname].X.dropna()[ind+1]) for ind,value in enumerate(rateofchange) if value <3e-3]
+    ax.plot(data[sampname].X.dropna(),data[sampname].Y,label=sampname)
+    # print(rateofchange)
+
+
 #find threshold
-xdata = [22.41495404,29.44332212,40.95373061,50.16001444,59.36940718,68.92260246,78.36003461,87.7974297,97.23502654,107.1006261,114.910408,123.2259297,131.6672342,141.3794975,152.3033352,164.1235812,176.6965228,188.864084,201.2749847,213.9367468]
-ydata = [3.235E-05,2.782E-05,2.11001E-05,1.55084E-05,1.02451E-05,1.44989E-05,1.99223E-05,2.53418E-05,3.07826E-05,2.78438E-05,2.21175E-05,1.62206E-05,1.02122E-05,4.47522E-06,6.13289E-06,7.98703E-06,1.03707E-05,9.42372E-06,7.38505E-06,5.04899E-06]
-diff = abs(np.diff(ydata))
-timestep = np.diff(xdata)
-rateofchange = diff/timestep
-print(rateofchange[16],rateofchange[17])
+# xdata = [22.41495404,29.44332212,40.95373061,50.16001444,59.36940718,68.92260246,78.36003461,87.7974297,97.23502654,107.1006261,114.910408,123.2259297,131.6672342,141.3794975,152.3033352,164.1235812,176.6965228,188.864084,201.2749847,213.9367468]
+# ydata = [3.235E-05,2.782E-05,2.11001E-05,1.55084E-05,1.02451E-05,1.44989E-05,1.99223E-05,2.53418E-05,3.07826E-05,2.78438E-05,2.21175E-05,1.62206E-05,1.02122E-05,4.47522E-06,6.13289E-06,7.98703E-06,1.03707E-05,9.42372E-06,7.38505E-06,5.04899E-06]
+# diff = abs(np.diff(ydata))
+# timestep = np.diff(xdata)
+# rateofchange = diff/timestep
+# print(rateofchange[16],rateofchange[17])
 # for ind,val in enumerate(ydata):
 
 
 
 
-fig,ax = plt.subplots()
-for sample in SSAporosityinfo.index:    
-    # if "Menke2017" not in SSAporosityinfo.Publication[sample]:continue
-    reff = [float(ele) for ele in SSAporosityinfo.reff[sample][1:-1].split(",")]
-    timesteps = [int(ele)*60 for ele in SSAporosityinfo.Timestep[sample][1:-1].split(",")] #in seconds
-    timestep = np.diff(timesteps[1::])
-    diff = abs(np.diff(reff))
-    rateof_change = diff/timestep
-    percentchange = rateof_change/reff[0]
-    ind = check_convergence(percentchange)
-    if ind == None: 
-        print(sample)
-    else: print(ind)
-    timesteps = [ele/60 for ele in timesteps]
-    ax.plot(timesteps[1::],reff,'-.', label=sample)
+# fig,ax = plt.subplots()
+if calc_rateofchange: 
+    for sample in SSAporosityinfo.index:    
+        if "AlKhulaifi2018" not in SSAporosityinfo.Publication[sample]:continue
+        # if "ket" not in sample: continue
+        reff = [float(ele) for ele in SSAporosityinfo.reff[sample][1:-1].split(",")]
+        timesteps = [int(ele) for ele in SSAporosityinfo.Timestep[sample][1:-1].split(",")] #in seconds
+        averagedreff = moving_average(reff)
+        changeiny = abs(np.diff(averagedreff))/averagedreff[0]
+        changeinx = abs(np.diff(timesteps[2:-1]))
+        rateofchange = changeiny/changeinx
+        # [print(sample,timesteps[ind+2]) for ind,value in enumerate(rateofchange) if value <3e-3]
+        ax.plot(timesteps[2:-1],averagedreff,label=sample)
+    # timestep = np.diff(timesteps[1::])
+    # diff = abs(np.diff(reff))
+    # rateof_change = diff/timestep
+    # percentchange = rateof_change/reff[0]
+    # ind = check_convergence(percentchange)
+    # if ind == None: 
+    #     print(sample)
+    # else: print(ind)
+    # timesteps = [ele/60 for ele in timesteps]
+    # ax.plot(timesteps[1::],reff,'-.', label=sample)
     
     # print(ind)
     # print("convergence at step ", ind, " with eff rxn rate ", reff[ind], "mol/m2s")
 #         print(ind)
 #         print("final rate of change:", (rateof_change)[-1]*100, "%")
-ax.set_ylim(0,1.2e-4)
-ax.set_xlabel("Time [min]")
-ax.set_ylabel("Effective Reaction Rate (reff) [mol/m2s]")
-ax.legend()
-fig.tight_layout()
-# plt.show()
-
-
-
+    ax.set_ylim(0,1.3e-5)
+    ax.set_xlabel("Time [min]")
+    ax.set_ylabel("Effective Reaction Rate (reff) [mol/m2s]")
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
 
 # reff=[]
 # for ind in range(1,len(porosity_time)):
